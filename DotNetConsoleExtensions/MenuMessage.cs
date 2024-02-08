4b31ddfc67e9cdb2c2509862ccd998246e7678ba
COGMLS <mlsmatheus@hotmail.com>
2024-02-08 11:45:41 -0300

    Fixed mixed line ending and changed spaces to tabs.

--- DotNetConsoleExtensions/MenuMessage.cs
+++ DotNetConsoleExtensions/MenuMessage.cs
@@ -2,68 +2,68 @@
 
 namespace DotNetConsoleExtensions
 {
-    public struct MenuMessage
-    {
-        public readonly string[] message;
-        public readonly ConsoleColor foregroundColor;
-        public readonly ConsoleColor backgroundColor;
+	public struct MenuMessage
+	{
+		public readonly string[] message;
+		public readonly ConsoleColor foregroundColor;
+		public readonly ConsoleColor backgroundColor;
 
-        public MenuMessage(string[] message)
-        {
-            this.message = message;
-            this.foregroundColor = Console.ForegroundColor;
-            this.backgroundColor = Console.BackgroundColor;
-        }
+		public MenuMessage(string[] message)
+		{
+			this.message = message;
+			this.foregroundColor = Console.ForegroundColor;
+			this.backgroundColor = Console.BackgroundColor;
+		}
 
-        public MenuMessage(string[] message, ConsoleColor foregroundColor)
-        {
-            this.message = message;
-            this.foregroundColor = foregroundColor;
-            this.backgroundColor = Console.BackgroundColor;
-        }
+		public MenuMessage(string[] message, ConsoleColor foregroundColor)
+		{
+			this.message = message;
+			this.foregroundColor = foregroundColor;
+			this.backgroundColor = Console.BackgroundColor;
+		}
 
-        public MenuMessage(string[] message, ConsoleColor foregroundColor, ConsoleColor backgroundColor)
-        {
-            this.message = message;
-            this.foregroundColor = foregroundColor;
-            this.backgroundColor = backgroundColor;
-        }
+		public MenuMessage(string[] message, ConsoleColor foregroundColor, ConsoleColor backgroundColor)
+		{
+			this.message = message;
+			this.foregroundColor = foregroundColor;
+			this.backgroundColor = backgroundColor;
+		}
 
-        public void Write()
-        {
-            ConsoleColor actualForegroundColor = Console.ForegroundColor;
-            ConsoleColor actualBackgroundColor = Console.BackgroundColor;
+		public void Write()
+		{
+			ConsoleColor actualForegroundColor = Console.ForegroundColor;
+			ConsoleColor actualBackgroundColor = Console.BackgroundColor;
 
-            string tmp = "";
+			string tmp = "";
 
-            for (int i = 0; i < this.message.Length; i++)
-            {
-                tmp += this.message[i];
-            }
+			for (int i = 0; i < this.message.Length; i++)
+			{
+				tmp += this.message[i];
+			}
 
-            Console.ForegroundColor = actualForegroundColor;
-            Console.BackgroundColor = actualBackgroundColor;
-        }
+			Console.ForegroundColor = actualForegroundColor;
+			Console.BackgroundColor = actualBackgroundColor;
+		}
 
-        public void WriteLine()
-        {
-            ConsoleColor actualForegroundColor = Console.ForegroundColor;
-            ConsoleColor actualBackgroundColor = Console.BackgroundColor;
+		public void WriteLine()
+		{
+			ConsoleColor actualForegroundColor = Console.ForegroundColor;
+			ConsoleColor actualBackgroundColor = Console.BackgroundColor;
 
-            string tmp = "";
+			string tmp = "";
 
-            for (int i = 0; i < this.message.Length; i++)
-            {
-                if (i != 0 && i < this.message.Length - 1)
-                {
-                    tmp += "\n";
-                }
+			for (int i = 0; i < this.message.Length; i++)
+			{
+				if (i != 0 && i < this.message.Length - 1)
+				{
+					tmp += "\n";
+				}
 
-                tmp += this.message[i];
-            }
+				tmp += this.message[i];
+			}
 
-            Console.ForegroundColor = actualForegroundColor;
-            Console.BackgroundColor = actualBackgroundColor;
-        }
-    }
+			Console.ForegroundColor = actualForegroundColor;
+			Console.BackgroundColor = actualBackgroundColor;
+		}
+	}
 }
