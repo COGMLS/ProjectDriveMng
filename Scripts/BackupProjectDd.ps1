94b9303877302aec002a7d63b82437880a747271
COGMLS <mlsmatheus@hotmail.com>
2024-02-23 12:39:29 -0300

    Added support to restore files

--- Scripts/BackupProjectDd.ps1
+++ Scripts/BackupProjectDd.ps1
@@ -39,6 +39,16 @@ Param
     [switch]
     $Backup,
 
+    # Restore a backup file. This parameter can be used with Backup, to set a backup before restore the file.
+    [Parameter(Mandatory = $false, Position = 0)]
+    [switch]
+    $Restore,
+
+    # Specify the date and time backup to restore. This parameter only have effect when Restore is used.
+    [Parameter(Mandatory = $false, Position = 1)]
+    [System.DateTime]
+    $DateTime,
+
     # List only backup. This option does not have effect if Backup parameter is send.
     [Parameter(Mandatory = $false, Position = 0)]
     [switch]
@@ -50,6 +60,8 @@ Param
 [string]$ProjectDbFileName = "$($ProjectDbBaseName)$($ProjectDbExtension)"
 [string]$ManagementFolder = "Gerenciamento"
 [string]$BackupDirName = "Backup"
+[string]$BackupSigniture = "_BACKUP-"
+[string]$DateTimeStringConverter = "yyyy-MM-dd HH-mm-ss"
 
 function MakeProjectDriveDbBackup
 {
@@ -64,7 +76,7 @@ function MakeProjectDriveDbBackup
             $BackupDirInfo.Create()
         }
 
-        [string]$ProjectDbBackupName = "$($ProjectDbFileInfo.BaseName)_BACKUP-$($ProjectDbFileInfo.LastWriteTime.ToString("yyyy-MM-dd HH-mm-ss"))$($ProjectDbFileInfo.Extension)"
+        [string]$ProjectDbBackupName = "$($ProjectDbFileInfo.BaseName)$($BackupSigniture)$($ProjectDbFileInfo.LastWriteTime.ToString($DateTimeStringConverter))$($ProjectDbFileInfo.Extension)"
 
         [string]$ProjectDbBackupPath = [System.IO.Path]::Combine($BackupDirInfo.FullName, $ProjectDbBackupName)
 
@@ -129,11 +141,125 @@ function ListProjectDriveDb
     $ProjectDbList | Format-Table -AutoSize
 }
 
+function RestoreProjectDriveDbBackup
+{
+    Param
+    (
+        [System.DateTime]$BackupDt = $null
+    )
+    
+    [string]$BackupDirPath = [System.IO.Path]::Combine($env:ProjectDrive, $ManagementFolder, $BackupDirName)
+    [System.IO.DirectoryInfo]$BackupDirInfo = [System.IO.DirectoryInfo]::new($BackupDirPath)
+
+    [System.IO.FileInfo]$OriginalProjectDriveDbFileInfo = [System.IO.FileInfo]::new([System.IO.Path]::Combine($env:ProjectDrive, $ManagementFolder, $ProjectDbFileName))
+
+    if ($BackupDirInfo.Exists)
+    {
+        [System.IO.FileInfo[]]$BackupFiles = $BackupDirInfo.GetFiles("$($ProjectDbBaseName)$($BackupSigniture)", [System.IO.SearchOption]::TopDirectoryOnly)
+        
+        if ($BackupFiles.Length -gt 0)
+        {
+            [System.IO.FileInfo]$Backup2Restore = $null
+
+            # Restore the most recent backup.
+            if ($null -eq $BackupDt)
+            {
+                $BackupFiles = $BackupFiles | Sort-Object -Property LastWriteTime -Descending
+
+                $Backup2Restore = $BackupFiles[0]
+            }
+            else
+            {
+                [string]$Dt2Str = $BackupDt.ToString($DateTimeStringConverter)
+                [string]$ProjectDriveDbNameSearch = "$($ProjectDbBaseName)$($BackupSigniture)$($Dt2Str)$($ProjectDbExtension)"
+
+                for ($i = 0; $i -lt $BackupFiles.Length; $i++)
+                {
+                    if ($BackupFiles[$i].Name -eq $ProjectDriveDbNameSearch)
+                    {
+                        $Backup2Restore = $BackupFiles[$i]
+                        break
+                    }
+                }
+            }
+
+            if ($null -ne $Backup2Restore)
+            {
+                if ($Backup2Restore.Exists)
+                {
+                    try
+                    {
+                        Copy-Item -Path $Backup2Restore -Destination $OriginalProjectDriveDbFileInfo.FullName -Verbose -Force -Confirm
+                    }
+                    catch
+                    {
+                        
+                    }
+
+                    [System.IO.FileInfo]$CopiedFile = [System.IO.FileInfo]::new($OriginalProjectDriveDbFileInfo.FullName)
+
+                    if ($CopiedFile.LastWriteTime -eq $Backup2Restore.LastWriteTime)
+                    {
+                        Write-Host -Object "DataBase $($Backup2Restore.Name) from $($Backup2Restore.LastWriteTime.ToString($DateTimeStringConverter)) was restored with success!" -ForegroundColor Green
+                    }
+                    else
+                    {
+                        Write-Host -Object "Fail in retore the DataBase file $($Backup2Restore.Name)!"
+                        Write-Host -Object "Backup to restore:"
+                        $Backup2Restore
+                        Write-Host -Object "Actual file:"
+                        $OriginalProjectDriveDbFileInfo
+                    }
+                }
+                else
+                {
+                    Write-Host -Object "The backup file $($Backup2Restore.Name) doesn't exist!"
+                }
+            }
+            else
+            {
+                Write-Host -Object "Fail to find the backup $($ProjectDriveDbNameSearch)"
+            }
+        }
+        else
+        {
+            Write-Host -Object "There is no backup file available to make the restore operation!"
+        }
+    }
+    else
+    {
+        Write-Host -Object "The backup folder doesn't exist!"
+    }
+}
+
 if ($Backup)
 {
     MakeProjectDriveDbBackup
+
+    if ($Restore)
+    {
+        if ($null -eq $DateTime)
+        {
+            RestoreProjectDriveDbBackup
+        }
+        else
+        {
+            RestoreProjectDriveDbBackup -BackupDt $DateTime
+        }
+    }
 }
-elseif (-not $Backup -and $ListOnlyBackups)
+elseif ($Restore)
+{
+    if ($null -eq $DateTime)
+    {
+        RestoreProjectDriveDbBackup
+    }
+    else
+    {
+        RestoreProjectDriveDbBackup -BackupDt $DateTime
+    }
+}
+elseif (-not $Backup -and -not $Restore -and $ListOnlyBackups)
 {
     ListProjectDriveDb -OnlyBackups
 }
