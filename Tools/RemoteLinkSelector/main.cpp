5c5347049faf2646bf12fee8998a759beae44f07
COGMLS <mlsmatheus@hotmail.com>
2024-08-10 14:10:27 -0300

    Added support for command objects. Added command queue. Added support for help command. Added support for list command. Added support for add command.

--- Tools/RemoteLinkSelector/main.cpp
+++ Tools/RemoteLinkSelector/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <locale>
 #include <filesystem>
 #include <stdexcept>
+#include <queue>
 
 #include <unistd.h>
 
@@ -8,12 +10,20 @@
 #include "places.hpp"
 #include "config.hpp"
 #include "directory.hpp"
+#include "commands.hpp"
+#include "menus.hpp"
 
 int main (int argc, const char* argv[], const char* argp[])
 {
+	std::locale loc("en_US.UTF-8");
+	std::locale::global(loc);
+
 	//size_t argpSize = 0;
 	//while (argp[++argpSize] != NULL);
 	//std::cout << "argp size: " << argpSize << std::endl;
+
+	// Check for user credentials:
+	// NOTE: no root user is allowed by this application!
 
 	__uid_t me = getuid();
 	__uid_t myprivs = geteuid();
@@ -31,6 +41,174 @@ int main (int argc, const char* argv[], const char* argp[])
 		std::cout << "This program is designed to run only with non root privileges!" << std::endl << "Aborting the application." << std::endl;
 		return -2;
 	}
+
+	// If the user credentials / privileges are correct, start the app:
+
+	std::queue<cmd> cmdQueue;
+	std::vector<std::string> cmdLineVec;
+
+	for (size_t i = 1; i < argc; i++)
+	{
+		//cmdLineVec.push_back(argv[i]);
+		cmdQueue.push(cmd(argv[i]));
+	}
+
+	// No arguments to work. Show the actual configuration:
+	if (cmdQueue.size() == 0)
+	{
+		auto links = getPlaces();
+		auto refLink = getRemoteRefSymLink(links);
+
+		if (refLink.empty())
+		{
+			std::cout << "No git remote server reference link is set." << std::endl;
+		}
+		else
+		{
+			std::cout << "Current git remote server reference link path: " << refLink.string() << std::endl;
+		}
+
+		return 2;
+	}
+
+	while (!cmdQueue.empty())
+	{
+		cmd c = cmdQueue.front();
+
+		if (c.getType() != ARGUMENT)
+		{
+			// Remove current command from queue
+			cmdQueue.pop();
+
+			switch (c.getType())
+			{
+				case CMDTYPE::HELP_CMD:
+				{
+					auto help = getHelp();
+
+					for (std::string& l : help)
+					{
+						std::cout << l << std::endl;
+					}
+
+					return 0;
+					//break;
+				}
+				case CMDTYPE::LIST_CMD:
+				{
+					if (cmdQueue.empty())
+					{
+						return 3;
+					}
+
+					if (cmdQueue.front().getType() == CMDTYPE::ARGUMENT)
+					{
+						cmd arg = cmdQueue.front();
+
+						if (arg.getValue() == "config")
+						{
+							if (remoteLinkSelectorDirExists())
+							{
+								std::vector<std::filesystem::path> configFiles = getConfigFiles();
+								std::vector<std::string> configList = showConfigsList(configFiles);
+
+								for (std::string& l : configList)
+								{
+									std::cout << l << std::endl;
+								}
+							}
+							else
+							{
+								std::cout << "Remote Link Selector doesn't exist!" << std::endl;
+								std::cout << "Create a new configuration to define the directory." << std::endl;
+							}
+						}
+						else if (arg.getValue() == "links")
+						{
+							if (placesDirExists())
+							{
+								std::vector<std::filesystem::path> placesLinks = getPlaces();
+
+								for (std::filesystem::path& p : placesLinks)
+								{
+									std::cout << p.filename().stem() << " -> " << std::filesystem::read_symlink(p).string() << std::endl;
+								}
+							}
+							else
+							{
+								std::cout << "Places directory doesn't exist!" << std::endl;
+								std::cout << "Create the directory in home's root to be able to access it." << std::endl;
+							}
+						}
+						else
+						{
+							return 4;
+						}
+					}
+					else
+					{
+						return 4;
+					}
+
+					break;
+				}
+				case CMDTYPE::ADD_CMD:
+				{
+					std::string computer, description, source;
+
+					std::cout << "Enter with the values for new configurations:" << std::endl;
+
+					std::cout << "Computer: ";
+					std::getline(std::cin, computer);
+					std::cout << "Source: ";
+					std::getline(std::cin, source);
+					std::cout << "Description: ";
+					std::getline(std::cin, description);
+
+					if (computer.empty() || source.empty())
+					{
+						return 5;
+					}
+
+					configData newCfg;
+					newCfg.computer = computer;
+					newCfg.description = description;
+					newCfg.source = source;
+					newCfg.version = 1;
+
+					int saveStatus = saveFile(newCfg);
+
+					if (saveFile >= 0)
+					{
+						std::cout << "Configurations saved with success." << std::endl;
+						return 0;
+					}
+					else
+					{
+						std::cout << "Fail to save your configurations. Exit Code: " << saveStatus << std::endl;
+						return 5;
+					}
+
+					break;
+				}
+				case CMDTYPE::REMOVE_CMD:
+				{
+					
+					break;
+				}
+				case CMDTYPE::SELECT_CMD:
+				{
+					break;
+				}
+				default:
+				{
+					break;
+				}
+			}
+		}
+	}
+
+	// ------------------------------------------------------------------- //
 
 	std::cout << "Check for Remote Link Selector dir: " << chkRemoteLinkSelectorDir() << std::endl;
 
