d837b36696f5f5e1645a2708c40e87a2efb302f5
COGMLS <mlsmatheus@hotmail.com>
2024-10-07 20:33:16 -0300

    Fixed missing Patch version. Added test with version information for every library. NOTE: Correct behavior for imported libraries include was not fixed.

--- ProjectDriveMng/Console/VersionDataBase.hpp
+++ ProjectDriveMng/Console/VersionDataBase.hpp
@@ -9,11 +9,30 @@
 #include "ProjectDriveMngVersion.hpp"
 #include "../Libs/Imported/VersionLib/VersionLib.hpp"
 
+#include "LoggerLibVersionTools.hpp"
+#include "ConsoleExtVersionTools.hpp"
+
 const std::array<std::string, 3> verMinDb = 
 {
 	"0.1.0",	// Console Extensions
 	"3.0.0", 	// LoggerLib
 	"0.8.0"	 	// VersionLib
 };
+
+VersionLib::VersionData ProjectDriveMng_vInfo(
+	PROJECT_DRIVE_MANAGER_MAJOR_VERSION,
+	PROJECT_DRIVE_MANAGER_MINOR_VERSION,
+	PROJECT_DRIVE_MANAGER_PATCH_VERSION,
+	PROJECT_DRIVE_MANAGER_BUILD_NUMBER,
+	VersionLib::BuildType::ALPHA,//VersionLib::str2BuildType(PROJECT_DRIVE_MANAGER_BUILD_TYPE),
+	PROJECT_DRIVE_MANAGER_REVISION_NUMBER
+);
+
+ConsoleExt::Version ConsoleExt_vStrcutInfo = ConsoleExt::getLibVersion();
+LoggerLib::Version LoggerLib_vStructInfo = LoggerLib::getLibVersion();
+
+VersionLib::VersionData ConsoleExt_vInfo(ConsoleExt::getVersionStr(ConsoleExt_vStrcutInfo, true, true));
+VersionLib::VersionData LoggerLib_vInfo(LoggerLib::getVersionStr(LoggerLib_vStructInfo, true, true));
+VersionLib::VersionData VersionLib_vInfo(VersionLib::internalVersionData);
 
 #endif // !VERSION_DATABASE_HPP
--- ProjectDriveMng/Console/main.cpp
+++ ProjectDriveMng/Console/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include "VersionDataBase.hpp"
+
+#include "../Libs/Imported/VersionLib/VersionLib.hpp"
+
 int main (int argc, const char* argv[], const char* argp[])
 {
-	std::cout << "Project Drive Manager Console" << std::endl;
+	std::cout << "Project Drive Manager Console\t" << ProjectDriveMng_vInfo.getVersionStr(false, false) << std::endl;
+
+	std::cout << std::endl << "Library versions:" << std::endl;
+	std::cout << "--------------------------------------------------------" << std::endl;
+
+	std::cout << "  Console Extensions: " << ConsoleExt_vInfo.getVersionStr() << std::endl;
+	std::cout << "  LoggerLib:          " << LoggerLib_vInfo.getVersionStr() << std::endl;
+	std::cout << "  VersionLib:         " << VersionLib_vInfo.getVersionStr() << std::endl;
 
 	return 0;
 }
